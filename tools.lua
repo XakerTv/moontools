4ebf6189d4d96d80ebb943cca33aef87f5d61dfb
XakerTv <101417914+XakerTv@users.noreply.github.com>
2024-12-07 20:19:05 +0300

    Add files via upload

--- tools.lua
+++ tools.lua
@@ -10,17 +10,18 @@ local inicfg = require 'inicfg'
 
 update_status = false
 
-local script_vers = 1
-local script_vers_text = "1.00"
+local script_vers = 2
+local script_vers_text = "1.01"
 
 local update_url = "https://raw.githubusercontent.com/XakerTv/moontools/refs/heads/main/update.ini"
 local update_path = getWorkingDirectory() .. "/update.ini"
 
-local script_url = ""
+local script_url = "https://github.com/XakerTv/moontools/raw/refs/heads/main/tools.lua"
 local script_path = thisScript().path
 
 scriptName = "{8B59FF}[ Luna Tools ]{FFFFFF}"
 betaScriptName = "[ Luna | DeBug ]"
+scriptVersion = "1a"
 
 function main()
     while not isSampAvailable() do wait(0) end
@@ -28,8 +29,8 @@ function main()
     local playerName = sampGetPlayerNickname(playerId)
     sampAddChatMessage(scriptName .. " Скрипт готов к работе.", 0xFFFFFF)
     sampAddChatMessage(scriptName .. " С возвращением, " .. playerName, 0xFFFFFF)
-    --sampAddChatMessage(scriptName .. " Открыть главное меню: /mtools", 0xFFFFFF)
-    --sampAddChatMessage(betaScriptName .. " Версия скрипта: " .. scriptVersion, 0xBFBFBF)
+    sampAddChatMessage(betaScriptName .. " Открыть главное меню: /mtools", 0xFFFFFF)
+    sampAddChatMessage(betaScriptName .. " Версия скрипта: " .. scriptVersion, 0xBFBFBF)
     --sampRegisterChatCommand('mtools', function()
     --    renderWindow[0] = not renderWindow[0]
     --end)
@@ -51,6 +52,10 @@ function main()
             downloadUrlToFile(update_url, update_path, function(id, status)
                 if status == dlstatus.STATUS_ENDDOWNLOADDATA then
                     sampAddChatMessage(scriptName .. 'Скрипт успешно обновлен!', -1)
+                    sampAddChatMessage(scriptName .. '==============ОБНОВЛЕНИЕ' .. scriptVersion .. '==============',
+                        0x8B59FF)
+                    sampAddChatMessage(scriptName .. '* Добавлено: *', -1)
+                    sampAddChatMessage(scriptName .. '- Функция автообновления', -1)
                     thisScript():reload()
                 end
             end)
