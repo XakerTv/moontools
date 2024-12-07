f15ea44a7c82227f2c5ef28468c6b10c881799f4
XakerTv <101417914+XakerTv@users.noreply.github.com>
2024-12-07 23:39:19 +0300

    Add files via upload

--- tools.lua
+++ tools.lua
@@ -6,8 +6,6 @@ script_version('1')
 require 'lib.moonloader'
 local dlstatus = require('moonloader').download_status
 local inicfg = require 'inicfg'
-local keys = require "vkeys"
-local imgui = require 'encoding'
 local encoding = require "encoding"
 
 encoding.default = 'CP1251'
@@ -41,32 +39,42 @@ function main()
     sampAddChatMessage(betaScriptName .. " Открыть главное меню: /mtools", 0xFFFFFF)
     sampAddChatMessage(betaScriptName .. " Версия скрипта: " .. scriptVersion, 0xBFBFBF)
 
+    while true do
+        wait(0)
+    end
+end
+
+function cmd_update()
+    sampAddChatMessage(scriptName .. 'Проверка обновлений...', 0xFFFFFF)
+
+    -- Загрузка файла update.ini
     downloadUrlToFile(update_url, update_path, function(id, status)
         if status == dlstatus.STATUS_ENDDOWNLOADDATA then
-            updateIni = inicfg.load(nil, update_path)
+            local updateIni = inicfg.load(nil, update_path)
             if tonumber(updateIni.info.vers) > script_vers then
                 sampAddChatMessage(scriptName .. 'Найдено обновление! Версия: ' .. updateIni.info.vers_text, -1)
                 update_state = true
+
+                -- Загрузка нового скрипта
+                downloadUrlToFile(script_url, script_path, function(id, status)
+                    if status == dlstatus.STATUS_ENDDOWNLOADDATA then
+                        sampAddChatMessage(scriptName .. 'Скрипт успешно обновлен!', -1)
+                        sampAddChatMessage(scriptName .. '==============ОБНОВЛЕНИЕ' .. scriptVersion .. '==============',
+                            0x8B59FF)
+                        sampAddChatMessage(scriptName .. '* Добавлено: *', -1)
+                        sampAddChatMessage(scriptName .. '- Функция автообновления', -1)
+                        thisScript():reload()
+                    else
+                        sampAddChatMessage(scriptName .. 'Ошибка загрузки нового скрипта.', 0xFF0000)
+                    end
+                end)
+            else
+                sampAddChatMessage(scriptName .. 'Обновлений не найдено. Текущая версия: ' .. script_vers_text, 0xFFFFFF)
             end
-            os.remove(update_path)
+
+            os.remove(update_path) -- Удаляем временный файл
+        else
+            sampAddChatMessage(scriptName .. 'Ошибка проверки обновлений.', 0xFF0000)
         end
     end)
-
-    while true do
-        wait(0)
-
-        if update_state then
-            downloadUrlToFile(script_url, script_path, function(id, status)
-                if status == dlstatus.STATUS_ENDDOWNLOADDATA then
-                    sampAddChatMessage(scriptName .. 'Скрипт успешно обновлен!', -1)
-                    sampAddChatMessage(scriptName .. '==============ОБНОВЛЕНИЕ' .. scriptVersion .. '==============',
-                        0x8B59FF)
-                    sampAddChatMessage(scriptName .. '* Добавлено: *', -1)
-                    sampAddChatMessage(scriptName .. '- Функция автообновления', -1)
-                    thisScript():reload()
-                end
-            end)
-            break
-        end
-    end
 end
