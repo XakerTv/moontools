6b02386e8aafce954c302fc0935b161a17d58548
XakerTv <101417914+XakerTv@users.noreply.github.com>
2024-12-07 23:02:18 +0300

    Add files via upload

--- tools.lua
+++ tools.lua
@@ -1,84 +1,71 @@
 script_name("LunaTools")
 script_author("HermitTech")
 script_description("Luna Tools. Vers. 1")
-script_version('2')
+script_version("1")
+
+require "lib.moonloader"
+require "lib.sampfuncs"
+local dlstatus = require('moonloader').download_status
+
+update_state = false -- Если переменная == true, значит начнётся обновление.
+update_found = false -- Если будет true, будет доступна команда /update.
+
+local script_vers = 1
+local script_vers_text =
+"1.0" -- Название нашей версии. В будущем будем её выводить ползователю.
+
+local update_url =
+'https://www.dropbox.com/scl/fi/1mifj2d4jorajt7h8x00z/update.ini?rlkey=i9gtvifpq77ovdviyykofkyoa&st=6h1bmzqo&dl=0' -- Путь к ini файлу. Позже нам понадобиться.
+local update_path = getWorkingDirectory() .. "/update.ini"
+
+local script_url =
+'https://www.dropbox.com/scl/fi/v9viclmaf7utsu1hqwlew/tools.lua?rlkey=y7nur7pnzh1t7imu7ptnmnb1u&st=v9m88tj0&dl=0' -- Путь скрипту.
+local script_path = thisScript().path
 
 scriptName = "{8B59FF}[ Luna Tools ]{FFFFFF}"
 betaScriptName = "[ Luna | DeBug ]"
 scriptVersion = "1a"
 
-require 'lib.moonloader'
-local encoding = require 'encoding'
-local requests = require 'requests'
-local dlstatus = require('moonloader').download_status
-
-encoding.default = 'CP1251'
-u8 = encoding.UTF8
-
--- Подключаем dkjson для работы с JSON
-local dkjson = require 'dkjson'
-
-function decodeJson(data)
-    return dkjson.decode(data)
-end
-
-function update()
-    local raw = 'https://raw.githubusercontent.com/XakerTv/moontools/refs/heads/main/updater.json'
-    local f = {}
-
-    function f:getLastVersion()
-        local response = requests.get(raw)
-        if response.status_code == 200 then
-            return decodeJson(response.text)['last']
-        else
-            return 'UNKNOWN'
+function check_update() -- Создаём функцию которая будет проверять наличие обновлений при запуске скрипта.
+    downloadUrlToFile(update_url, update_path, function(id, status)
+        if status == dlstatus.STATUS_ENDDOWNLOADDATA then
+            updateIni = inicfg.load(nil, update_path)
+            if tonumber(updateIni.info.vers) > script_vers then -- Сверяем версию в скрипте и в ini файле на github
+                sampAddChatMessage(
+                    scriptName ..
+                    "{FFFFFF}Имеется {32CD32}новая {FFFFFF}версия скрипта. Версия: {32CD32}" ..
+                    updateIni.info.vers_text .. ". {FFFFFF}/update что-бы обновить", 0xFF0000) -- Сообщаем о новой версии.
+                update_found = true -- если обновление найдено, ставим переменной значение true
+            end
+            os.remove(update_path)
         end
-    end
-
-    function f:download()
-        local response = requests.get(raw)
-        if response.status_code == 200 then
-            local tempPath = getWorkingDirectory() .. '\\temp_script.lua'
-            downloadUrlToFile(decodeJson(response.text)['url'], tempPath, function(id, status)
-                if status == dlstatus.STATUSEX_ENDDOWNLOAD then
-                    os.rename(tempPath, thisScript().path) -- Заменяем текущий скрипт
-                    sampAddChatMessage(scriptName .. ' Скрипт обновлен. Перезагрузка...', -1)
-                    thisScript():reload()
-                elseif status == dlstatus.STATUSEX_ERROR then
-                    sampAddChatMessage(scriptName .. ' Ошибка скачивания обновления.', -1)
-                end
-            end)
-        else
-            sampAddChatMessage(scriptName .. ' Ошибка: невозможно установить обновление. Код: ' .. response.status_code,
-                -1)
-        end
-    end
-
-    return f
+    end)
 end
 
 function main()
-    while not isSampAvailable() do wait(0) end
+    if not isSampLoaded() or not isSampfuncsLoaded() then return end
+    while not isSampAvailable() do wait(100) end
 
-    -- Автообновление
-    local updater = update()
-    local lastVersion = updater:getLastVersion()
-    if lastVersion ~= scriptVersion then
-        sampAddChatMessage(scriptName .. " Доступна новая версия скрипта: " .. lastVersion, 0xFFFF00)
-        updater:download()
-        return
+    check_update()
+
+    if update_found then                             -- Если найдено обновление, регистрируем команду /update.
+        sampRegisterChatCommand('update', function() -- Если пользователь напишет команду, начнётся обновление.
+            update_state = true                      -- Если человек пропишет /update, скрипт обновится.
+        end)
+    else
+        sampAddChatMessage('{FFFFFF}Нету доступных обновлений!')
     end
-
-    -- Основная функциональность
-    _, id = sampGetPlayerIdByCharHandle(PLAYER_PED)
-    nick = sampGetPlayerNickname(id)
-
-    sampAddChatMessage(scriptName .. " Скрипт готов к работе.", 0xFFFFFF)
-    sampAddChatMessage(scriptName .. " С возвращением, " .. nick, 0xFFFFFF)
-    sampAddChatMessage(betaScriptName .. " Открыть главное меню: /mtools", 0xFFFFFF)
-    sampAddChatMessage(betaScriptName .. " Версия скрипта: " .. scriptVersion, 0xBFBFBF)
 
     while true do
         wait(0)
+
+        if update_state then -- Если человек напишет /update и обновление есть, начнётся скачивание скрипта.
+            downloadUrlToFile(script_url, script_path, function(id, status)
+                if status == dlstatus.STATUS_ENDDOWNLOADDATA then
+                    sampAddChatMessage(scriptName .. "{FFFFFF}Скрипт {32CD32}успешно {FFFFFF}обновлён.", 0xFF0000)
+                end
+            end)
+            break
+        end
     end
 end
