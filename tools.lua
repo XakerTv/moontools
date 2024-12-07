51fa8caa286b321a154441f40619b389e083aa77
XakerTv <101417914+XakerTv@users.noreply.github.com>
2024-12-07 22:34:00 +0300

    Add files via upload

--- tools.lua
+++ tools.lua
@@ -1,33 +1,68 @@
 script_name("LunaTools")
 script_author("HermitTech")
 script_description("Luna Tools. Vers. 1")
-script_version("1")
+script_version('2')
 
 scriptName = "{8B59FF}[ Luna Tools ]{FFFFFF}"
 betaScriptName = "[ Luna | DeBug ]"
 scriptVersion = "1a"
 
 require 'lib.moonloader'
-local dlstatus = require('moonloader').download_status
-local inicfg = require 'inicfg'
 local encoding = require 'encoding'
+local requests = require 'requests'
+local dlstatus = require('moonloader').download_status
+
 encoding.default = 'CP1251'
 u8 = encoding.UTF8
 
-update_state = false
+function update()
+    local raw = 'https://raw.githubusercontent.com/XakerTv/moontools/refs/heads/main/updater.json'
+    local f = {}
 
-local script_vers = 2
-local script_vers_text = '2.00'
+    function f:getLastVersion()
+        local response = requests.get(raw)
+        if response.status_code == 200 then
+            return decodeJson(response.text)['last']
+        else
+            return 'UNKNOWN'
+        end
+    end
 
-local update_url = 'https://raw.githubusercontent.com/XakerTv/moontools/refs/heads/main/update.ini'
-local update_path = getWorkingDirectory() .. '/update.ini'
+    function f:download()
+        local response = requests.get(raw)
+        if response.status_code == 200 then
+            downloadUrlToFile(decodeJson(response.text)['url'], thisScript().path, function(id, status, p1, p2)
+                print('Скачиваю ' .. decodeJson(response.text)['url'] .. ' в ' .. thisScript().path)
+                if status == dlstatus.STATUSEX_ENDDOWNLOAD then
+                    sampAddChatMessage('Скрипт обновлен, перезагрузка...', -1)
+                    thisScript():reload()
+                    sampAddChatMessage(scriptName .. '==============ОБНОВЛЕНИЕ' .. scriptVersion .. '==============',
+                        0x8B59FF)
+                    sampAddChatMessage(scriptName .. '* Добавлено: *', -1)
+                    sampAddChatMessage(scriptName .. '- Функция автообновления', -1)
+                end
+            end)
+        else
+            sampAddChatMessage('Ошибка, невозможно установить обновление, код: ' .. response.status_code, -1)
+        end
+    end
 
-local script_url = 'https://github.com/XakerTv/moontools/raw/refs/heads/main/tools.lua'
-local script_path = thisScript().path
+    return f
+end
 
 function main()
     while not isSampAvailable() do wait(0) end
 
+    -- Автообновление
+    local updater = update()
+    local lastVersion = updater:getLastVersion()
+    if lastVersion ~= script_version() then
+        sampAddChatMessage(scriptName .. " Доступна новая версия скрипта: " .. lastVersion, 0xFFFF00)
+        updater:download()
+        return
+    end
+
+    -- Основная функциональность
     _, id = sampGetPlayerIdByCharHandle(PLAYER_PED)
     nick = sampGetPlayerNickname(id)
 
@@ -36,40 +71,7 @@ function main()
     sampAddChatMessage(betaScriptName .. " Открыть главное меню: /mtools", 0xFFFFFF)
     sampAddChatMessage(betaScriptName .. " Версия скрипта: " .. scriptVersion, 0xBFBFBF)
 
-    downloadUrlToFile(update_url, update_path, function(id, status)
-        if status == dlstatus.STATUS_ENDDOWNLOADDATA then
-            if doesFileExist(update_path) then
-                updateIni = inicfg.load(nil, update_path)
-                if updateIni and tonumber(updateIni.info.vers) > script_vers then
-                    sampAddChatMessage(scriptName .. ' Есть обновление скрипта! Устанавливаем...', 0xff0000)
-                    update_state = true
-                else
-                    sampAddChatMessage(scriptName .. ' Ошибка: некорректный файл обновления.', 0xff0000)
-                end
-                os.remove(update_path)
-            else
-                sampAddChatMessage(scriptName .. ' Ошибка: файл обновления не найден.', 0xff0000)
-            end
-        else
-            sampAddChatMessage(scriptName .. ' Ошибка: не удалось скачать файл обновления.', 0xff0000)
-        end
-    end)
-
     while true do
         wait(0)
-
-        if update_state then
-            downloadUrlToFile(script_url, script_path, function(id, status)
-                if status == dlstatus.STATUS_ENDDOWNLOADDATA then
-                    sampAddChatMessage('Обновление скачано!', 0xff0000)
-                    sampAddChatMessage(scriptName .. '==============ОБНОВЛЕНИЕ' .. scriptVersion .. '==============',
-                        0x8B59FF)
-                    sampAddChatMessage(scriptName .. '* Добавлено: *', -1)
-                    sampAddChatMessage(scriptName .. '- Функция автообновления', -1)
-                    thisScript():reload()
-                end
-            end)
-            break
-        end
     end
 end
